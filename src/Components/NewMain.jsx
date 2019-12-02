b23baa951fbffd6b4026f76826f61132b2ea0812
r105 <ronaldbeltre05@gmail.com>
2019-12-02 09:25:43 -0400

    Continue working

--- src/Components/NewMain.jsx
+++ src/Components/NewMain.jsx
@@ -35,24 +35,107 @@ class NewMain extends Component {
             b: 20,
             m: 23
           },
-          nPrice: {
-            a: 150,
-            b: 245,
-            m: 360
-          },
+          nPrice: 37500,
           rDuration: {
             a: 10,
             b: 17,
             m: 20
           },
-          rPrice: {
-            a: 110,
-            b: 230,
-            m: 295
-          }
+          rPrice: 25000
+        },
+        {
+          name: "B",
+          prerequisites: [],
+          nDuration: {
+            a: 15,
+            b: 20,
+            m: 23
+          },
+          nPrice: 37500,
+          rDuration: {
+            a: 10,
+            b: 17,
+            m: 20
+          },
+          rPrice: 25000
         }
+        // {
+        //   name: "C",
+        //   prerequisites: [],
+        //   nDuration: {
+        //     a: 15,
+        //     b: 20,
+        //     m: 23
+        //   },
+        //   nPrice: 37500,
+        //   rDuration: {
+        //     a: 10,
+        //     b: 17,
+        //     m: 20
+        //   },
+        //   rPrice: 25000
+        // },
+        // {
+        //   name: "D",
+        //   prerequisites: [],
+        //   nDuration: {
+        //     a: 15,
+        //     b: 20,
+        //     m: 23
+        //   },
+        //   nPrice: 37500,
+        //   rDuration: {
+        //     a: 10,
+        //     b: 17,
+        //     m: 20
+        //   },
+        //   rPrice: 25000
+        // }
       ]
+      // prerequisites: ["ZX"]
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.createNewActivity = this.createNewActivity.bind(this);
+  }
+
+  handleChange(event) {
+    let id = event.target.id;
+    let stateCopy = { ...this.state };
+
+    if (id.substring(1, 9) === "Duration") {
+      let propt1 = id.substring(0, id.indexOf("."));
+      let propt2 = id.substring(
+        id.indexOf(".") + 1,
+        parseInt(id.substring(id.indexOf("-")))
+      );
+      let index = parseInt(id.substring(id.indexOf("-") + 1));
+      debugger;
+      stateCopy.myActivities[index][propt1][propt2] = event.target.value;
+    } else {
+      let propt = id.substring(0, id.indexOf("-"));
+      let index = parseInt(id.substring(id.indexOf("-") + 1));
+
+      stateCopy.myActivities[index][propt] = event.target.value;
+    }
+
+    this.setState({
+      ...stateCopy
+    });
+  }
+
+  createNewActivity() {
+    this.setState({
+      ...this.state.myActivities.push({
+        name: "",
+        prerequisites: [],
+        nDuration: {},
+        nPrice: 0,
+        rDuration: {},
+        rPrice: 0
+      })
+    });
+    console.log(this.state);
   }
 
   render() {
@@ -73,87 +156,117 @@ class NewMain extends Component {
                 <TableCell className="tr">Te R</TableCell>
                 <TableCell className="tr">Vte N</TableCell>
                 <TableCell className="tr">Vte r</TableCell>
-                <TableCell className="tr">Importe</TableCell>
-                <TableCell className="tr">Eliminar</TableCell> */}
+                <TableCell className="tr">Importe</TableCell>*/}
+                <TableCell className="tr">Eliminar</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {this.state.myActivities.map(activity => {
+              {this.state.myActivities.map((activity, index) => {
                 return (
-                  <TableRow>
+                  <TableRow key={index}>
                     <TableCell>
                       <TextField
-                        value={activity.name}
+                        id={`name-${index}`}
+                        value={this.state.myActivities[index].name}
+                        // value={this.state.timeUnit}
+                        onChange={this.handleChange}
                         //   onChange={}
                       ></TextField>
                     </TableCell>
                     <TableCell>
-                      <Select
-                      //   value={act.pre} // Values already Selected
-                      // onChange={}
-                      //   renderValue={selected => <div>{selected + ''}</div>} // The way that already selected values will be rendered
-                      >
-                        {/* Handles Values in the Selection Menu */}
-                        {/* {data.map(
-                      ({ name }) =>
-                        name !== act.name &&
-                        isValueInAnotherArray(data[index].pre, name) && (
-                          <MenuItem value={name}>{name}<Result/MenuItem>
-                        )
-                    )} */}
-                      </Select>
+                      {index != 0 && (
+                        <Select
+                          display={index !== 0 ? "block" : "none"}
+                          // value={index !== 0 ? this.state.myActivities : null} // Values already Selected
+                          value={"Select a P"}
+                        >
+                          {/* Handles Values in the Selection Menu */}
+                          {this.state.myActivities.map(
+                            ({ name }, ind) => {
+                              return (
+                                name !== activity.name &&
+                                index > ind && (
+                                  <MenuItem value={name} key={ind}>
+                                    {name}
+                                  </MenuItem>
+                                )
+                              );
+                            }
+
+                            // isValueInAnotherArray(data[index].pre, name) && (
+                            //   <MenuItem value={name}>{name}<Result/MenuItem>
+                            // )
+                          )}
+                        </Select>
+                      )}
                     </TableCell>
                     <TableCell>
                       <TextField
-                        // label="A"/
-                        value={activity.rDuration.a}
-                      ></TextField>
-                      <TextField
-                        // label="M"
-                        // type="number"
-                        value={activity.rDuration.b}
-                      ></TextField>
-                      <TextField
-                        // label="B"
-                        // type="number"
-                        value={activity.rDuration.m}
-                      ></TextField>
-                    </TableCell>
-                    <TableCell>
-                      <TextField
-                        type="number"
-                        value={activity.nPrice.a}
-                        // onChange={event => handleChange(event, "cost", index)}
-                      ></TextField>
-                    </TableCell>
-                    <TableCell>
-                      <TextField
+                        id={`nDuration.a-${index}`}
                         label="A"
-                        // onChange={event => handleChange(event, "a", index)}
-                      ></TextField>
-                      <TextField
-                        label="M"
                         type="number"
-                        // onChange={event => handleChange(event, "m", index)}
+                        value={activity.nDuration.a}
+                        onChange={this.handleChange}
                       ></TextField>
                       <TextField
+                        id={`nDuration.b-${index}`}
                         label="B"
                         type="number"
+                        value={activity.nDuration.b}
+                        onChange={this.handleChange}
+                      ></TextField>
+                      <TextField
+                        id={`nDuration.m-${index}`}
+                        label="M"
+                        type="number"
+                        value={activity.nDuration.m}
+                        onChange={this.handleChange}
+                      ></TextField>
+                    </TableCell>
+                    <TableCell>
+                      <TextField
+                        id={`nPrice-${index}`}
+                        type="number"
+                        value={activity.nPrice}
+                        onChange={this.handleChange}
+                      ></TextField>
+                    </TableCell>
+                    <TableCell>
+                      <TextField
+                        id={`rDuration.a-${index}`}
+                        label="A"
+                        value={activity.nDuration.a}
+                        onChange={this.handleChange}
+                      ></TextField>
+                      <TextField
+                        id={`rDuration.b-${index}`}
+                        label="B"
+                        type="number"
+                        value={activity.rDuration.b}
+                        onChange={this.handleChange}
+                      ></TextField>
+                      <TextField
+                        id={`rDuration.m-${index}`}
+                        label="M"
+                        type="number"
+                        value={activity.rDuration.m}
+                        onChange={this.handleChange}
                         // onChange={event => handleChange(event, "b", index)}
                       ></TextField>
                     </TableCell>
                     <TableCell>
                       <TextField
+                        id={`rPrice-${index}`}
                         type="number"
-                        // value={act.cost}
-                        // onChange={event => handleChange(event, "cost", index)}
+                        value={activity.rPrice}
+                        onChange={this.handleChange}
                       ></TextField>
                     </TableCell>
-                    <TableCell> {"duration"}</TableCell>
+                    {/* <TableCell> {"duration"}</TableCell>
                     <TableCell></TableCell>
                     <TableCell></TableCell>
                     <TableCell></TableCell>
-                    <TableCell></TableCell>
+                    <TableCell></TableCell> */}
                     <TableCell>
                       <IconButton
                       //   onClick={() => removeActivity(data[index].name)}
@@ -169,8 +282,19 @@ class NewMain extends Component {
         </Grid>
 
         <Grid item xs={12}>
-          <Result timeUnit={this.state.timeUnit} />
+          <IconButton onClick={this.createNewActivity}>
+            Añadir
+            <AddIcon />
+          </IconButton>
+          <IconButton>
+            Calcular
+            <SendIcon />
+          </IconButton>
         </Grid>
+
+        {/* <Grid item xs={12}>
+          <Result timeUnit={this.state.timeUnit} />
+        </Grid> */}
 
         {/* <Grid item xs={12}>
           <Budget />
